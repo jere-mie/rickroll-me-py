a6ddf7858ca48dff1f538e84d2a0f94241afc140
jere-mie <jere-mie@users.noreply.github.com>
2020-08-08 00:20:05 -0400

    it works now

--- website/forms.py
+++ website/forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired, ValidationError
 
 class LinkForm(FlaskForm):
     link = StringField('Custom URL Ending', validators=[DataRequired()])
+    url = StringField('URL to Redirect To', validators=[DataRequired()])
     title = StringField('Site Title', validators=[DataRequired()])
     name = StringField('Site Name', validators=[DataRequired()])
     desc = StringField('Description', validators=[DataRequired()])
--- website/routes.py
+++ website/routes.py
@@ -3,38 +3,19 @@ from website import app, db
 from website.forms import LinkForm
 from website.models import Link
 
-# from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
 def home():
     return render_template('home.html')
 
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if current_user.is_authenticated:
-#         return redirect(url_for('home'))
-#     form = Login()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.username.data).first()
-#         if user and form.password.data==user.password:
-#             login_user(user, remember=form.remember.data)
-#             return redirect(url_for('home'))
-#         else:
-#             flash('Error Logging In', 'danger')
-#     return render_template("login.html", form=form)
 
-# @app.route('/logout')
-# def logout():
-#     logout_user()
-#     return redirect(url_for('home'))
 
 @app.route('/new', methods=['GET', 'POST'])
-@login_required
 def new():
-    form = Link()
+    form = LinkForm()
     if form.validate_on_submit():
-        link = Link(link=form.link.data, title=form.title.data, name = form.name.data, desc=form.desc.data, image=form.image.data)
+        link = Link(link=form.link.data, title=form.title.data, name = form.name.data, desc=form.desc.data, image=form.image.data, url=form.url.data)
         db.session.add(link)
         db.session.commit()
         flash(f'Created link exampledomain.tld/{form.link.data}!', 'success')
@@ -42,12 +23,12 @@ def new():
     return render_template('new.html', form=form, legend='New Link')
 
 @app.route('/<link_url>/edit', methods=['GET', 'POST'])
-@login_required
 def edit(link_url):
-    link = Link.query.get_or_404(link_url)
+    link = Link.query.filter_by(link=link_url).first()
     form = LinkForm()
     if form.validate_on_submit():
         link.link = form.link.data
+        link.url = form.url.data
         link.title = form.title.data
         link.name = form.name.data
         link.desc = form.desc.data
@@ -58,6 +39,7 @@ def edit(link_url):
         return redirect(url_for('home'))
     elif request.method == 'GET':
         form.link.data = link.link
+        form.url.data = link.url
         form.title.data = link.title
         form.name.data = link.name
         form.desc.data = link.desc
@@ -67,5 +49,5 @@ def edit(link_url):
 
 @app.route('/l/<link_url>', methods=['GET'])
 def redir(link_url):
-    link = Link.query.get_or_404(link_url)
-    return render_template('redir.html', name=link.name, title=link.title, desc=link.desc, image=link.image, url=link.link)
+    link = Link.query.filter_by(link=link_url).first()
+    return render_template('redir.html', title=link.title, name=link.name, desc=link.desc, image=link.image, url=link.url)
